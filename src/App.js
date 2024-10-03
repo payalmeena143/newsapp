ff2b498bd45db1dee3c24b3ddf43751768372f55
NitinVadadoriya <155728096+NitinVadadoriyaa@users.noreply.github.com>
2024-10-03 14:31:02 +0530

    for india-country api outdated so made change from `in` to `us' in api

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ apiKey = process.env.REACT_APP_NEWS_API;
         <Router>
           <Navbar />
          <Routes>
-         <Route path="/" element={<News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" />} />
-         <Route path="/business" element={<News apiKey={this.apiKey}  key="business" pageSize={this.pageSize} country="in" category="business" />} />
-         <Route path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
-         <Route path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" />} />
-         <Route path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health" />} />
-         <Route path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science" />} />
-         <Route path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
-         <Route path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
+         <Route path="/" element={<News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="us" category="general" />} />
+         <Route path="/business" element={<News apiKey={this.apiKey}  key="business" pageSize={this.pageSize} country="us" category="business" />} />
+         <Route path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="us" category="entertainment" />} />
+         <Route path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="us" category="general" />} />
+         <Route path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="us" category="health" />} />
+         <Route path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="us" category="science" />} />
+         <Route path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="us" category="sports" />} />
+         <Route path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="us" category="technology" />} />
            
             </Routes>
         </Router>
